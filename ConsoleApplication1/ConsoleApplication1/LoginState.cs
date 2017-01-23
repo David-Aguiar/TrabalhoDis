c36919184f539f918eb5849aa93d0d6622dfdf0a
David Aguiar <2023612@student.uma.pt>
2017-01-23 17:20:04 +0000

    update

---
+++ ConsoleApplication1/ConsoleApplication1/LoginState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class LoginState
+    {
+        #region Singleton - usage: LoginState.GetInstance()
+
+        private static readonly LoginState Instance = new LoginState();
+
+        static LoginState()
+        {
+        }
+
+        private LoginState()
+        {
+        }
+
+        public static LoginState GetInstance()
+        {
+            return Instance;
+        }
+
+        #endregion
+
+        public static void LoginUser()
+        {
+            LoginConection.CheckConection();
+        }
+    }
+}
