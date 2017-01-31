a37a63d6520cc2d5e13dcfa2da22a9a029449e95
Rúben França <ruben_tiagof@hotmail.com>
2017-01-31 15:38:35 +0000

    Criação da ligação à base de dados singleton

---
+++ ConsoleApplication1/ConsoleApplication1/ComunicacaoBD.cs
@@ -0,0 +1,38 @@
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+namespace ConsoleApplication1
+{
+
+    public class Conection
+    {
+		private static ComunicacaoBD instance;
+		
+		private ComunicacaoBD() {}
+		
+		public static ComunicacaoBD Instance
+		{
+			get
+			{
+				if(instance == null)
+				{
+					instance = new ComunicacaoBD();
+				}
+				return instance;
+			}
+		}
+		
+		private ComunicaBD BD = new ComunicaBD();
+		
+		
+		
+		
+		
+	}
+}
---
+++ ConsoleApplication1/ConsoleApplication1/comunicaBD.cs
@@ -0,0 +1,39 @@
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+namespace ConsoleApplication1
+{
+	public class comunicaBD
+	{
+		private MySqlConnection Conexao = new MySqlConnection("host=127.0.0.1; user=root; database=mydb");
+		
+		public ComunicaBD()
+		{
+			try
+			{
+				Conexao.Open();
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show(e.Message, "Erro na ligação à base de dados", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+		
+		public void FechaConexao()
+		{
+			try
+			{
+				Conexao.Close();
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show(e.Message, "Erro ao se desligar da base de dados", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+	}
