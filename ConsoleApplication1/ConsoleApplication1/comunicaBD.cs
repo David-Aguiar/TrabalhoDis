6e648a5ef080d3406cd38d950c59c6af5ffc39a3
Rúben França <ruben_tiagof@hotmail.com>
2017-01-31 16:08:31 +0000

    criação de método global para queries

--- ConsoleApplication1/ConsoleApplication1/comunicaBD.cs
+++ ConsoleApplication1/ConsoleApplication1/comunicaBD.cs
@@ -9,7 +9,7 @@ using System.Data;
 
 namespace ConsoleApplication1
 {
-	public class comunicaBD
+	public class ComunicaBD
 	{
 		private MySqlConnection Conexao = new MySqlConnection("host=127.0.0.1; user=root; database=mydb");
 		
@@ -35,5 +35,24 @@ namespace ConsoleApplication1
 			{
 				MessageBox.Show(e.Message, "Erro ao se desligar da base de dados", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
+		}
+		
+		public DataTable Query(String query)
+		{
+			DataTable dataTable = new DataTable();
+			MySqlAdapter adapter;
+			
+			try
+			{
+				MySqlCommand command = new MySqlCommand(query, Conexao);
+				adapter = new MySqlAdapter(command);
+				adapter.Fill(dataTable);
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show(e.Message, "Erro ao executar query na base de dados", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(query, "Query passada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			return dataTable;
 		}
 	}
